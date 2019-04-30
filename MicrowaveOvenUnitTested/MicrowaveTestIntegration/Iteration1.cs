3c97eb9fc8f1ae8a75c8831e14f459cc4ac6011e
carolinekdl <caroline.kaagaard.dahl.laursen@post.au.dk>
2019-04-30 11:56:45 +0200

    Test of iteration 1 Cookcontroller, PowerTube, Timer and Display

--- MicrowaveOvenUnitTested/MicrowaveTestIntegration/Iteration1.cs
+++ MicrowaveOvenUnitTested/MicrowaveTestIntegration/Iteration1.cs
@@ -8,41 +8,118 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using NSubstitute;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
 
 namespace MicrowaveTestIntegration
 {
-    class Iteration1SUT
+    [TestFixture]
+    public class Iteration1SUT
     {
-        private ITimer fakeTimer;
+        //Fakes
+        private ITimer fakeTimer; //Lavet som fake, fordi det ellers ville tage for lang tid at køre testprogrammet
         private IOutput fakeOutput;
         private IUserInterface fakeUserInterface;
+        private IDoor fakeDoor;
+        private ILight fakeLight;
 
-        private Display display;
-        private PowerTube powerTube;
+
+        //System under test
+        private IDisplay display;
+        private IPowerTube powerTube;
         private CookController cookController;
 
-        private EventArgs _timertick; 
+        //buttons
+        private IButton powerButton;
+        private IButton timeButton;
+        private IButton startCancelButton;
+        
 
-        //Vi tror vi skal have et "private" et eller andet med et event her
-
+  
         [SetUp]
         public void setUp()
         {
-            fakeTimer = Substitute.For<ITimer>();
+            //Fakes
+            fakeTimer = Substitute.For<ITimer>(); //Lavet som fake, fordi det ellers ville tage for lang tid at køre testprogrammet
             fakeUserInterface = Substitute.For<IUserInterface>();
             fakeOutput = Substitute.For<IOutput>();
+            fakeDoor = new Door();
+            fakeLight = new Light(fakeOutput);
+
+            fakeUserInterface = new UserInterface(powerButton, timeButton, startCancelButton, fakeDoor, display, fakeLight, cookController);
+
+            //System under test
             display = new Display(fakeOutput);
-            cookController = new CookController(fakeTimer, display, powerTube, fakeUserInterface);
-            fakeUserInterface = Substitute.For<IUserInterface>();
-            _timertick = new EventArgs();
-            //Og noget mere med det event her
+            cookController = new CookController(fakeTimer, display, powerTube);
+            cookController.UI = fakeUserInterface;
+
+            powerButton = Substitute.For<IButton>();
+            timeButton = Substitute.For<IButton>();
+            startCancelButton = Substitute.For<IButton>();
+        }
+        
+        #region Display
+
+        [Test] 
+        public void cookController_display_showtime_test()
+        {
+            powerButton.Pressed += Raise.Event();
+            timeButton.Pressed += Raise.Event();
+            startCancelButton.Pressed += Raise.Event(); //The raise of these three events puts the microwave oven into cooking state
+
+            fakeTimer.TimerTick += Raise.Event();
+
+            fakeOutput.Received().OutputLine(Arg.Is<string>(str => str.Contains("Display shows: "))); //This tests that the display shows something, when OnTimerTick event is raised
         }
 
-        [Test] //Vi vil gerne teste at display viser timeren, når timeren startes
-        public void cookcotroler_display_Integration_Test()
+        #endregion
+
+        #region PowerTube
+
+        [Test]
+        public void cookController_PowerTube_TurnOn()
         {
-            cookController.OnTimerTick(null, null); //åh nej hvad skal der stå?
+            int power = 50;
+
+            powerButton.Pressed += Raise.Event();
+            timeButton.Pressed += Raise.Event(); //The raise of these two events puts the microwave oven into set time state
+
+            cookController.StartCooking(power, TimeSpan.FromSeconds(60));
+
+            fakeOutput.Received().OutputLine(Arg.Is<string>(str => str.Contains($"PowerTube works with {power} W")));
+
         }
+
+        [Test]
+        public void cookController_PowerTube_TurnOff()
+        {
+            powerButton.Pressed += Raise.Event();
+            timeButton.Pressed += Raise.Event();
+            startCancelButton.Pressed += Raise.Event(); //The raise of these three events puts the microwave oven into cooking state
+
+            cookController.Stop();
+
+            fakeOutput.Received().OutputLine(Arg.Is<string>(str => str.Contains("PowerTube turned off")));
+        }
+
+        [Test]
+        public void cookController_TimerExpired_PowerTube_TurnOff()
+        {
+            powerButton.Pressed += Raise.Event();
+            timeButton.Pressed += Raise.Event();
+            startCancelButton.Pressed += Raise.Event(); //The raise of these three events puts the microwave oven into cooking state
+
+            fakeTimer.Expired += Raise.Event();
+
+            fakeOutput.Received().OutputLine(Arg.Is<string>(str => str.Contains("PowerTube turned off")));
+        }
+
+        #endregion
+
+        #region timer
+
+        //timer test
+
+        #endregion
 
 
     }
---
+++ MicrowaveOvenUnitTested/MicrowaveTestIntegration/Iteration3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NUnit;
+using NSubstitute;
+
+namespace MicrowaveTestIntegration
+{
+    [TestFixture]
+    public class Iteration3
+    {
+        //Fakes
+        private ITimer fakeTimer; //Lavet som fake, fordi det ellers ville tage for lang tid at køre testprogrammet
+        private IOutput fakeOutput;
+        private IDoor fakeDoor;
+        private CookController cookController;
+
+
+        //System under test
+        private IDisplay display;
+        private IPowerTube powerTube;
+        
+        private IUserInterface sut;
+        private ILight light;
+
+        //buttons
+        private IButton powerButton;
+        private IButton timerButton;
+        private IButton startCancelButton;
+    }
+}
